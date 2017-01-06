b0d76ab586f43b2c8d4e14f844df2df3929cd36d
Slava Babin <vbabin@bsoft.biz>
2017-01-06 22:02:12 +0200

    NullPointer

---
+++ src/main/java/NullPointer.java
@@ -0,0 +1,14 @@
+public class NullPointer{
+ 
+ 
+ public static void show(){
+ 
+        System.out.println("Static method called");
+ }
+ 
+ public static void main(String[] args)
+ {
+     NullPointer obj = null;
+     obj.show();
+    }
+}
